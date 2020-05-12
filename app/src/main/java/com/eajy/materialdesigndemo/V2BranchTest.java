b75909218d2bbc2531545252390194e089a44fc5
Wong <2062077878@qq.com>
2020-05-12 16:17:08 +0800

    测试分支V2

---
+++ app/src/main/java/com/eajy/materialdesigndemo/V2BranchTest.java
@@ -0,0 +1,10 @@
+package com.eajy.materialdesigndemo;
+
+/**
+ * Copyright (C) 2017-2020  Inc.All Rights Reserved.
+ * Created by WHW on 2020/5/12.
+ * Decription
+ */
+public class V2BranchTest {
+    //V2分支测试
+}
