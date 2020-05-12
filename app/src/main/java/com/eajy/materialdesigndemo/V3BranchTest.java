753177d1519b2ab85d4ee30f92691941b1b54719
Wong <2062077878@qq.com>
2020-05-12 23:47:25 +0800

    删除

--- app/src/main/java/com/eajy/materialdesigndemo/V3BranchTest.java
+++
@@ -1,9 +0,0 @@
-package com.eajy.materialdesigndemo;
-
-/**
- * Copyright (C) 2017-2020  Inc.All Rights Reserved.
- * Created by WHW on 2020/5/12.
- * Decription
- */
-public class V3BranchTest {
-}
